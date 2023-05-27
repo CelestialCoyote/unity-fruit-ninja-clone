4f02c41eefa3c47341710897f7d52774cb752571
Paul Stearns <celestialcoyote@gmail.com>
2023-05-26 17:31:53 -0700

    add models and materials

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+
+    public GameObject slicedFruitPrefab;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            CreateSlicedFruit();
+        }
+    }
+
+
+    public void CreateSlicedFruit(){
+
+        GameObject inst = (GameObject)Instantiate(slicedFruitPrefab, transform.position, transform.rotation);
+
+
+        Rigidbody[] rbsOnSliced = inst.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody r in rbsOnSliced){
+            r.transform.rotation = Random.rotation;
+            r.AddExplosionForce(Random.Range(500, 1000), transform.position, 5f);
+        }
+
+
+        Destroy(gameObject);
+    }
+
+
+}
